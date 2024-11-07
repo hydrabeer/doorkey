79425acf68573ed25aa3900346435aed07ce48bc
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2024-11-07 18:39:52 -0500

    Create Cipher.java abstract interface
    Abstract interface for performing encryption/decryption given a password and an input string

---
+++ src/main/java/interface_adapter/crypto/cipher/Cipher.java
@@ -0,0 +1,22 @@
+package interface_adapter.crypto.cipher;
+
+import org.bouncycastle.crypto.InvalidCipherTextException;
+
+public interface Cipher {
+    /**
+     * Encrypt the given input string with the given password.
+     * @param password the password string
+     * @param input the input string to encrypt
+     * @return a byte array containing the encrypted data
+     */
+    public byte[] encrypt(String password, String input);
+
+    /**
+     * Decrypt the given ciphertext with the provided password.
+     * @param password the password string
+     * @param input the byte array of encrypted bytes to decrypt
+     * @return the original plaintext string as provided to encrypt()
+     * @throws InvalidCipherTextException if the password is incorrect or input is invalid
+     */
+    public String decrypt(String password, byte[] input) throws InvalidCipherTextException;
+}
