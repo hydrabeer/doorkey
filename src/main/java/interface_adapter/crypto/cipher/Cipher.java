42500766e6895b2072c5813a332136073f59e05c
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2024-11-07 19:18:52 -0500

    Update Cipher.java javadoc with note about usage
    Single use; must create a new Cipher() every time you want to encrypt() or decrypt()
    
    This is because the cipher will internally store a state that would cause inconsistencies.

--- src/main/java/interface_adapter/crypto/cipher/Cipher.java
+++ src/main/java/interface_adapter/crypto/cipher/Cipher.java
@@ -5,6 +5,7 @@ import org.bouncycastle.crypto.InvalidCipherTextException;
 public interface Cipher {
     /**
      * Encrypt the given input string with the given password.
+     * <hr><strong>Note: you must create a new Cipher() for every encryption/decryption operation!</strong>
      * @param password the password string
      * @param input the input string to encrypt
      * @return a byte array containing the encrypted data
@@ -13,6 +14,7 @@ public interface Cipher {
 
     /**
      * Decrypt the given ciphertext with the provided password.
+     * <hr><strong>Note: you must create a new Cipher() for every encryption/decryption operation!</strong>
      * @param password the password string
      * @param input the byte array of encrypted bytes to decrypt
      * @return the original plaintext string as provided to encrypt()
