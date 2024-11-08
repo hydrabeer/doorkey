6833ec75bdd7a5f9bd10b89cfe54de8526e77df7
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-11-07 20:56:37 -0500

    Implemented Temporary BasicRequirementsCheck

--- src/main/java/password_validation/requirements/BasicRequirementsCheck.java
+++ src/main/java/password_validation/requirements/BasicRequirementsCheck.java
@@ -1,5 +1,27 @@
 package password_validation.requirements;
 
-public class BasicRequirementsCheck {
+public class BasicRequirementsCheck implements RequirementsCheck {
 
+    private boolean requireUpper;
+    private boolean requireLower;
+    private boolean requireSpecial;
+    private boolean requireNumeric;
+    private int minLength;
+    private int maxLength;
+
+    public BasicRequirementsCheck(boolean requireUpper, boolean requireLower, boolean requireSpecial,
+            boolean requireNumeric, int minLength, int maxLength) {
+        this.requireUpper = requireUpper;
+        this.requireLower = requireLower;
+        this.requireSpecial = requireSpecial;
+        this.requireNumeric = requireNumeric;
+        this.minLength = minLength;
+        this.maxLength = maxLength;
+    }
+
+    @Override
+    public boolean meetsRequirements(String password) {
+        // Incomplete
+        return false;
+    }
 }
