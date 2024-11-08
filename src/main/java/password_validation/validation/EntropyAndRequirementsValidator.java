eceba04be882499e20c426816cc63d7d12e612f4
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-11-07 21:03:04 -0500

    Temporary Implementation of EntropyAndRequirementsValidator

--- src/main/java/password_validation/validation/EntropyAndRequirementsValidator.java
+++ src/main/java/password_validation/validation/EntropyAndRequirementsValidator.java
@@ -1,5 +1,20 @@
 package password_validation.validation;
 
-public class EntropyAndRequirementsValidator {
+import password_validation.entropy.EntropyCheck;
+import password_validation.requirements.RequirementsCheck;
 
+public class EntropyAndRequirementsValidator implements PasswordValidator {
+
+    private final RequirementsCheck requirementsCheck;
+    private final EntropyCheck entropyCheck;
+
+    public EntropyAndRequirementsValidator(RequirementsCheck requirementsCheck, EntropyCheck entropyCheck) {
+        this.requirementsCheck = requirementsCheck;
+        this.entropyCheck = entropyCheck;
+    }
+
+    @Override
+    public boolean validate(String password) {
+        return requirementsCheck.meetsRequirements(password) && entropyCheck.calculateEntropy(password) > 3.0;
+    }
 }
