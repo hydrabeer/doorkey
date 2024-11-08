148640f57f290fc6eefedb0173cd2fecee95662f
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-11-07 20:57:16 -0500

    Implemented Temporary StatisticalEntropyCheck

--- src/main/java/password_validation/entropy/StatisticalEntropyCheck.java
+++ src/main/java/password_validation/entropy/StatisticalEntropyCheck.java
@@ -1,5 +1,16 @@
 package password_validation.entropy;
 
-public class StatisticalEntropyCheck {
+public class StatisticalEntropyCheck implements EntropyCheck {
 
+    private double entropyThreshold;
+
+    public StatisticalEntropyCheck(double entropyThreshold) {
+        this.entropyThreshold = entropyThreshold;
+    }
+
+    @Override
+    public double calculateEntropy(String password) {
+        // Incomplete
+        return 0;
+    }
 }
