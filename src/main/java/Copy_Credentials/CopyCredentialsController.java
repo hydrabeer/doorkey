8e111d04d074e27b7fea7f32d2c2fe1474472dc0
9naama <amaan.baweja@gmail.com>
2024-11-09 23:37:24 -0500

    Added basic instance variables where currently appropriate for basic structure

--- src/main/java/Copy_Credentials/CopyCredentialsController.java
+++ src/main/java/Copy_Credentials/CopyCredentialsController.java
@@ -6,14 +6,17 @@ import entity.VaultItem;
  * The Copy Credentials Interactor.
  */
 public class CopyCredentialsController {
+    private static CopyCredentialsInteractor copyCredentialsInteractor;
+
+    public CopyCredentialsController(CopyCredentialsInteractor copyCredentialsInteractor) {
+        this.copyCredentialsInteractor = copyCredentialsInteractor;
+    }
 
     public static void copyUsernameClicked(VaultItem vaultItem){
-        CopyCredentialsInteractor copyCredentialsInteractor = new CopyCredentialsInteractor();
         copyCredentialsInteractor.copyUsername(vaultItem);
     }
 
     public static void copyPasswordClicked(VaultItem vaultItem){
-        CopyCredentialsInteractor copyCredentialsInteractor = new CopyCredentialsInteractor();
         copyCredentialsInteractor.copyPassword(vaultItem);
     }
 }
--- src/main/java/Copy_Credentials/CopyCredentialsView.java
+++ src/main/java/Copy_Credentials/CopyCredentialsView.java
@@ -3,12 +3,17 @@ package Copy_Credentials;
 import entity.VaultItem;
 
 public class CopyCredentialsView {
+   private static CopyCredentialsController controller;
+
+   public CopyCredentialsView(CopyCredentialsController controller) {
+       this.controller = controller;
+   }
     public static void onCopyUsernameButtonClicked(){
         //Call to Controller
-//        CopyCredentialsController.copyUsernameClicked();
+//        controller.copyUsernameClicked();
     }
     public static void onCopyPasswordButtonClicked(){
 //        Call to Controller
-//        CopyCredentialsController.copyPasswordClicked();
+//        controller.copyPasswordClicked();
     }
 }
