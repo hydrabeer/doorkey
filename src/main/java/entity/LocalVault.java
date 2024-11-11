855896c3c29585c0fd96aeba864a62df08aa9a3e
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2024-11-11 15:24:19 -0500

    Add tests for Vault.toJSON and loadFromJSON

--- src/main/java/entity/LocalVault.java
+++ src/main/java/entity/LocalVault.java
@@ -5,7 +5,6 @@ import java.util.List;
 /**
  * LocalVault is a vault that stores items in JSON format.
  */
-
 public class LocalVault extends AbstractVault {
     public LocalVault(List<AbstractVaultItem> items) {
         super(items);
