820eefb0666c0137c7b6a5cd63660b88fb4bb839
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-11-11 10:49:14 -0500

    Password Template code implemented

--- src/main/java/password_validation/entity/Password.java
+++ src/main/java/password_validation/entity/Password.java
@@ -1,5 +1,18 @@
 package password_validation.entity;
 
+/**
+ * A simple implementation of a Password class.
+ */
 public class Password {
+
+    private final String value;
+
+    public Password(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
 
 }
