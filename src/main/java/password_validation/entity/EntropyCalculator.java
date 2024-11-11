0dff3d8c6f9d459577aad684ba7e2062561d08aa
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-11-11 10:48:39 -0500

    Base EntropyCalculator template code developed

--- src/main/java/password_validation/entity/EntropyCalculator.java
+++ src/main/java/password_validation/entity/EntropyCalculator.java
@@ -1,5 +1,18 @@
 package password_validation.entity;
 
+/**
+ * The implementation for Entropy Calculation.
+ */
 public class EntropyCalculator {
 
+    /**
+     * The algorithm for calculaing entropy.
+     *
+     * @param password the password for which we are calculating entropy.
+     * @return the final value of entropy/strength
+     */
+    public double calculateEntropy(String password) {
+        return 0.0;
+
+    }
 }
