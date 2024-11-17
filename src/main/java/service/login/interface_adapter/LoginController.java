a8785d1a117316d3f1fa62e045207d6d2840d3b5
Barış <bayazitbaris@gmail.com>
2024-11-17 03:40:36 -0500

    implement controller

---
+++ src/main/java/service/login/interface_adapter/LoginController.java
@@ -0,0 +1,35 @@
+package service.login.interface_adapter;
+
+import service.login.LoginInputBoundary;
+import service.login.LoginInputData;
+
+/**
+ * Controller for signing in.
+ */
+public class LoginController {
+    private final LoginInputBoundary interactor;
+
+    public LoginController(LoginInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    /**
+     * Executes the login.
+     * @param email The user's email.
+     * @param password The user's password.
+     */
+    public void login(String email, String password) {
+        final LoginInputData inputData = new LoginInputData(email, password);
+        interactor.login(inputData);
+    }
+
+    /**
+     * Switch to home view.
+     *
+     * @param email The user's email
+     * @param password The user's master password
+     */
+    public void switchToHomeView(String email, String password) {
+        interactor.switchToHomeView(email, password);
+    }
+}
