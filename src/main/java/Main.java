c2a6886a8931b85b412e33c18a191da28e6396f1
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2024-11-18 13:48:53 -0500

    Simplify Main.java anonymous fn

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,9 +19,7 @@ public class Main {
      * @param args command line arguments
      */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            initializeMainUi();
-        });
+        SwingUtilities.invokeLater(Main::initializeMainUi);
     }
 
     /**
