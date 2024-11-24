f6d975143afc36ed03005d1e66fc8e302ec45a7c
Barış <bayazitbaris@gmail.com>
2024-11-24 18:26:02 -0500

    Fix entity test

--- src/test/java/entity/CommonUserTest.java
+++ src/test/java/entity/CommonUserTest.java
@@ -9,19 +9,19 @@ import org.junit.jupiter.api.Test;
 
 public class CommonUserTest {
     private static final String title = "Google";
-    private static final String username = "doorkey@gmail.com";
+    private static final String email = "doorkey@gmail.com";
     private static final String password = "pa55w0rD";
     private static final String url = "https://google.com";
     private final List<AbstractVaultItem> items = new ArrayList<>();
 
     @Test
     void testCommonUser() {
-        PasswordVaultItem item = new PasswordVaultItem(title, username, password, url);
+        PasswordVaultItem item = new PasswordVaultItem(title, email, password, url);
         items.add(item);
 
         AbstractVault vault = new LocalVault(items);
-        AbstractUser user = new CommonUser(username, vault);
-        assertTrue(user.getUsername().equals(username));
+        AbstractUser user = new CommonUser(email, vault);
+        assertTrue(user.getEmail().equals(email));
         assertTrue(user.getVault() == vault);
         assertTrue(user.getVault().getItems().get(0) == item);
     }
