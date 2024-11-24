fbe107d3382e4b060dc9e9c427934c8b50bd95da
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-11-23 23:35:07 -0500

    Final version of PasswordValidationResponseModel

--- src/main/java/password_validation/use_case/PasswordValidationResponseModel.java
+++ src/main/java/password_validation/use_case/PasswordValidationResponseModel.java
@@ -1,5 +1,57 @@
 package password_validation.use_case;
 
+/**
+ * The response model for the password validation use case. Carries validation
+ * results from the interactor to the presenter.
+ */
 public class PasswordValidationResponseModel {
 
+    private final boolean meetsRequirements;
+    private final int entropy;
+    private final boolean isValid;
+    private final boolean lengthReq;
+    private final boolean upperLowerReq;
+    private final boolean numericReq;
+    private final boolean specialCharReq;
+
+    public PasswordValidationResponseModel(boolean meetsRequirements, int entropy, boolean isValid,
+            boolean lengthReq, boolean upperLowerReq,
+            boolean numericReq, boolean specialCharReq) {
+
+        this.meetsRequirements = meetsRequirements;
+        this.entropy = entropy;
+        this.isValid = isValid;
+        this.lengthReq = lengthReq;
+        this.upperLowerReq = upperLowerReq;
+        this.numericReq = numericReq;
+        this.specialCharReq = specialCharReq;
+    }
+
+    public boolean isMeetsRequirements() {
+        return meetsRequirements;
+    }
+
+    public int getEntropy() {
+        return entropy;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public boolean isLengthReq() {
+        return lengthReq;
+    }
+
+    public boolean isUpperLowerReq() {
+        return upperLowerReq;
+    }
+
+    public boolean isNumericReq() {
+        return numericReq;
+    }
+
+    public boolean isSpecialCharReq() {
+        return specialCharReq;
+    }
 }
