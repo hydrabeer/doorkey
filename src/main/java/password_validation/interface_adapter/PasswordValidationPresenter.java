c7dfadd7dde9ed682e7d8fd9c1441a8308fc9a91
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-11-23 23:40:46 -0500

    deleted unnecessary files

--- src/main/java/password_validation/interface_adapter/PasswordValidationPresenter.java
+++
@@ -1,7 +0,0 @@
-package interface_adapter.crypto.password_validation.presenter;
-
-import password_validation.use_case.PasswordValidationOutputBoundary;
-
-public class PasswordValidationPresenter implements PasswordValidationOutputBoundary {
-
-}
--- src/main/java/password_validation/interface_adapter/PasswordValidationViewModel.java
+++
@@ -1,5 +0,0 @@
-package interface_adapter.crypto.password_validation.view_model;
-
-public class PasswordValidationViewModel {
-
-}
