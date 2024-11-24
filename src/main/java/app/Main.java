f99e0e1012f810f04e0343f72d1b6b48fcea20f5
Barış <bayazitbaris@gmail.com>
2024-11-24 17:57:50 -0500

    Refactor with builder

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,37 @@
+package app;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import views.ViewConstants;
+
+/**
+ * The main class for our program.
+ */
+public class Main {
+    /**
+     * The main method that starts the GUI.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::initializeMainUi);
+    }
+
+    /**
+     * Initializes the main UI.
+     */
+    private static void initializeMainUi() {
+        final JFrame mainFrame = new AppBuilder("DoorKey", 400, 550)
+                .addHomeView()
+                .addLoginView()
+                .addSignupView()
+                .addLoadLocalVaultView()
+                .addCreateLocalVaultView()
+                .addLocalVaultView()
+                .setInitialView(ViewConstants.LOGIN_VIEW)
+                .build();
+
+        mainFrame.setVisible(true);
+    }
+}
