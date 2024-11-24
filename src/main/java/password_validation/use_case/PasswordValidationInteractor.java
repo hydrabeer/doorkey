fa084f789286b73f797b453d5faff2ac97bd2bea
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-11-23 23:33:52 -0500

    Final version of PasswordValidationInteractor

--- src/main/java/password_validation/use_case/PasswordValidationInteractor.java
+++ src/main/java/password_validation/use_case/PasswordValidationInteractor.java
@@ -1,5 +1,68 @@
 package password_validation.use_case;
 
+import java.util.regex.Pattern;
+
+import com.nulabinc.zxcvbn.Strength;
+import com.nulabinc.zxcvbn.Zxcvbn;
+
+/**
+ * The interactor for the password validation use case. Implements the
+ * application-specific business rules for validating passwords.
+ */
 public class PasswordValidationInteractor implements PasswordValidationInputBoundary {
 
+    private final PasswordValidationOutputBoundary presenter;
+
+    public PasswordValidationInteractor(PasswordValidationOutputBoundary presenter) {
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void validate(PasswordValidationRequestModel requestModel) {
+        final String password = requestModel.getPassword();
+        final boolean enforceEntropy = requestModel.enforceEntropy();
+
+        final boolean lengthReq = password.length() >= 8;
+        final boolean upperLowerReq = Pattern.compile("[A-Z]").matcher(password).find() 
+            && Pattern.compile("[a-z]").matcher(password).find();
+        final boolean numericReq = Pattern.compile("\\d").matcher(password).find();
+        final boolean specialCharReq = Pattern.compile("[!@#$%^&*(),.?\\\":{}|<>]").matcher(password).find();
+
+        final int entropy = calculateEntropy(password);
+
+        final boolean meetsRequirements = lengthReq && upperLowerReq && numericReq && specialCharReq && entropy > 2;
+
+        final boolean isValid;
+        if (enforceEntropy) {
+            isValid = meetsRequirements;
+        } 
+        else {
+            // Always valid during vault item creation/editing
+            isValid = true;
+        }
+
+        final PasswordValidationResponseModel responseModel = new PasswordValidationResponseModel(
+                meetsRequirements,
+                entropy,
+                isValid,
+                lengthReq,
+                upperLowerReq,
+                numericReq,
+                specialCharReq
+        );
+
+        presenter.present(responseModel);
+    }
+
+    /**
+     * Calculates the entropy of the password.
+     *
+     * @param password The password to analyze.
+     * @return The calculated entropy score.
+     */
+    private int calculateEntropy(String password) {
+        final Zxcvbn zxcvbn = new Zxcvbn();
+        final Strength strength = zxcvbn.measure(password);
+        return strength.getScore();
+    }
 }
