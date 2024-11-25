0ff813d9e506b1954eb1282ac0ec0ccef8d7bda0
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2024-11-25 10:44:11 -0500

    LoadLocalVaultPresenter: redirect to HomeView

--- src/main/java/service/local/load/interface_adapter/LoadLocalVaultPresenter.java
+++ src/main/java/service/local/load/interface_adapter/LoadLocalVaultPresenter.java
@@ -1,8 +1,15 @@
 package service.local.load.interface_adapter;
 
+import data_access.LocalVaultUserDataAccessObject;
+import entity.AbstractUser;
+import exception.AuthException;
+import repository.UserRepository;
 import service.ViewManagerModel;
+import service.home.interface_adapter.HomeState;
+import service.home.interface_adapter.HomeViewModel;
 import service.local.load.LoadLocalVaultOutputBoundary;
 import service.local.load.LoadLocalVaultOutputData;
+import views.ViewConstants;
 
 /**
  * Presents the load local vault view model.
@@ -10,20 +17,38 @@ import service.local.load.LoadLocalVaultOutputData;
 public class LoadLocalVaultPresenter implements LoadLocalVaultOutputBoundary {
     private final LoadLocalVaultViewModel loadLocalVaultViewModel;
     private final ViewManagerModel viewManagerModel;
+    private final HomeViewModel homeViewModel;
 
     public LoadLocalVaultPresenter(
         LoadLocalVaultViewModel loadLocalVaultViewModel,
+        HomeViewModel homeViewModel,
         ViewManagerModel viewManagerModel
     ) {
         this.loadLocalVaultViewModel = loadLocalVaultViewModel;
+        this.homeViewModel = homeViewModel;
         this.viewManagerModel = viewManagerModel;
     }
 
     @Override
     public void prepareSuccessView(LoadLocalVaultOutputData loadLocalVaultOutputData) {
-        System.out.println("Got data:" + loadLocalVaultOutputData.getVaultAsString());
-        // TODO @Evan: go to main view
-        System.out.println("TODO: GO TO MAIN VIEW ONCE IMPL.");
+        final HomeState currentHomeViewState = homeViewModel.getState();
+
+        UserRepository repository = new LocalVaultUserDataAccessObject();
+        try {
+            AbstractUser user = repository.signInUser(loadLocalVaultOutputData.getPath(), loadLocalVaultOutputData.getPassword());
+
+            currentHomeViewState.setUser(user);
+            currentHomeViewState.setUserRepository(new LocalVaultUserDataAccessObject());
+
+            homeViewModel.setState(currentHomeViewState);
+            homeViewModel.onStateChanged();
+
+            this.viewManagerModel.setState(ViewConstants.HOME_VIEW);
+            this.viewManagerModel.onStateChanged();
+        } catch (AuthException e) {
+            prepareErrorView(e.getMessage());
+            return;
+        }
     }
 
     @Override
