784959354d4666e3e0d888b8ce0997d2d34003a2
hydrabeer <65182259+hydrabeer@users.noreply.github.com>
2024-11-24 19:17:21 -0500

    Add getters and setters and the noResultsMessage field

--- src/main/java/service/search/interface_adapter/SearchState.java
+++ src/main/java/service/search/interface_adapter/SearchState.java
@@ -11,6 +11,7 @@ public class SearchState {
 
     private String query;
     private List<AbstractVaultItem> items;
+    private String noResultsMessage;
 
     public List<AbstractVaultItem> getItems() {
         return items;
@@ -18,5 +19,21 @@ public class SearchState {
 
     public void setItems(List<AbstractVaultItem> items) {
         this.items = items;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getNoResultsMessage() {
+        return noResultsMessage;
+    }
+
+    public void setNoResultsMessage(String noResultsMessage) {
+        this.noResultsMessage = noResultsMessage;
     }
 }
