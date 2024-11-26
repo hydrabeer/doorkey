8da69ff5e086610f1872ede4defeb745cafce955
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2024-11-26 01:38:45 -0500

    fix checkstyle

--- src/main/java/data_access/LocalVaultUserDataAccessObject.java
+++ src/main/java/data_access/LocalVaultUserDataAccessObject.java
@@ -23,43 +23,48 @@ public class LocalVaultUserDataAccessObject implements UserRepository {
     /**
      * Signs up a user by local vault path and password.
      *
-     * @param path    The path to the local .doorkey vault.
-     * @param password The password of the user.
+     * @param filepath    The path to the local .doorkey vault.
+     * @param vaultPassword The password of the user.
      * @return The user that was signed up.
      * @throws AuthException Never.
      */
     @Override
-    public AbstractUser signupUser(String path, String password) throws AuthException {
-        this.path = path;
-        this.password = password;
+    public AbstractUser signupUser(String filepath, String vaultPassword) throws AuthException {
+        this.path = filepath;
+        this.password = vaultPassword;
         return new LocalUser(new LocalVault(new ArrayList<>(), this.path, this.password));
     }
 
     /**
      * Logs in a user by local vault path and password.
      *
-     * @param path    The path to the local .doorkey vault.
-     * @param password The password of the user.
+     * @param filepath    The path to the local .doorkey vault.
+     * @param vaultPassword The password of the user.
      * @return The user that was logged in.
      * @throws AuthException If vault loading or decryption fails (such as bad password).
      */
     @Override
-    public AbstractUser signInUser(String path, String password) throws AuthException {
-        this.path = path;
-        this.password = password;
+    public AbstractUser signInUser(String filepath, String vaultPassword) throws AuthException {
+        this.path = filepath;
+        this.password = vaultPassword;
 
         // Read the vault as JSON
         try {
-            String content = new String(java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(path)));
+            final String content = new String(java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(path)));
 
-            LocalVault vault = new LocalVault(new ArrayList<>(), this.path, this.password);
+            final LocalVault vault = new LocalVault(new ArrayList<>(), this.path, this.password);
             vault.loadFromJSON(content);
 
             return new LocalUser(vault);
-        } catch (java.nio.file.NoSuchFileException e) {
+        }
+        catch (java.nio.file.NoSuchFileException exception) {
             throw new AuthException(AuthErrorReason.REQUEST_ERROR, "File not found: " + this.path);
-        } catch (Exception e) {
-            throw new AuthException(AuthErrorReason.UNKNOWN, "Failed to sign in user: " + e.getMessage());
+        }
+        catch (java.io.IOException exception) {
+            throw new AuthException(AuthErrorReason.UNKNOWN, "Failed to sign in user: " + exception.getMessage());
+        }
+        catch (exception.InvalidVaultItemException exception) {
+            throw new AuthException(AuthErrorReason.WRONG_CREDENTIALS, "Failed to load vault item; wrong password?");
         }
     }
 
@@ -79,11 +84,12 @@ public class LocalVaultUserDataAccessObject implements UserRepository {
         user.getVault().addItem(item);
 
         // Need to write to the local path again
-        String exported = user.getVault().toJSON();
+        final String exported = user.getVault().toJSON();
         try {
             java.nio.file.Files.write(java.nio.file.Paths.get(this.path), exported.getBytes());
-        } catch (Exception e) {
-            throw new AuthException(AuthErrorReason.UNKNOWN, "Failed to add vault item: " + e.getMessage());
+        }
+        catch (java.io.IOException exception) {
+            throw new AuthException(AuthErrorReason.UNKNOWN, "Failed to add vault item: " + exception.getMessage());
         }
     }
 }
