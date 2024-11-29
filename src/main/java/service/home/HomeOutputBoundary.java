31d93daf3cb09e2432d457e67eb84e0f8cf41581
9naama <amaan.baweja@gmail.com>
2024-11-28 20:13:37 -0500

    checkstyle fix

--- src/main/java/service/home/HomeOutputBoundary.java
+++ src/main/java/service/home/HomeOutputBoundary.java
@@ -6,10 +6,11 @@ import exception.InvalidVaultItemException;
  * The Home output boundary.
  */
 public interface HomeOutputBoundary {
+
     /**
      * Prepares the show vault view.
-     *
      * @param homeOutputData The home output data.
+     * @throws InvalidVaultItemException If the vault item is invalid.
      */
     void prepareShowVaultView(HomeOutputData homeOutputData) throws InvalidVaultItemException;
 
