0a5599a28246d3b62175be436d3fec460bf76a65
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-11-30 18:30:28 -0500

    Modified HomePresenter further to integrate vault item creation

--- src/main/java/service/home/interface_adapter/HomePresenter.java
+++ src/main/java/service/home/interface_adapter/HomePresenter.java
@@ -1,9 +1,15 @@
 package service.home.interface_adapter;
 
+import java.util.Optional;
+
+import entity.AbstractUser;
 import entity.AbstractVaultItem;
 import entity.PasswordVaultItem;
 import exception.InvalidVaultItemException;
+import repository.UserRepository;
 import service.ViewManagerModel;
+import service.create_vault_item.interface_adapter.CreateVaultItemState;
+import service.create_vault_item.interface_adapter.CreateVaultItemViewModel;
 import service.home.HomeOutputBoundary;
 import service.home.HomeOutputData;
 import service.password_vault_item.interface_adapter.PasswordVaultItemState;
@@ -18,14 +24,17 @@ public class HomePresenter implements HomeOutputBoundary {
     private final HomeViewModel homeViewModel;
     private final PasswordVaultItemViewModel passwordVaultItemViewModel;
     private final ViewManagerModel viewManagerModel;
+    private final CreateVaultItemViewModel createVaultItemViewModel;
 
     public HomePresenter(
             HomeViewModel homeViewModel,
             PasswordVaultItemViewModel passwordVaultItemViewModel,
+            CreateVaultItemViewModel createVaultItemViewModel,
             // note vault item view model
             ViewManagerModel viewManagerModel
     ) {
         this.homeViewModel = homeViewModel;
+        this.createVaultItemViewModel = createVaultItemViewModel;
         this.passwordVaultItemViewModel = passwordVaultItemViewModel;
         this.viewManagerModel = viewManagerModel;
     }
@@ -61,9 +70,18 @@ public class HomePresenter implements HomeOutputBoundary {
 
     @Override
     public void displayCreateVaultItemView() {
-        homeViewModel.setState(new HomeState());
-        homeViewModel.onStateChanged();
-        this.viewManagerModel.setState(ViewConstants.CREATE_VAULT_ITEM_VIEW);
-        this.viewManagerModel.onStateChanged();
+        final Optional<AbstractUser> user = homeViewModel.getState().getUser();
+        final Optional<UserRepository> userRepository = homeViewModel.getState().getUserRepository();
+        if (user.isPresent() && userRepository.isPresent()) {
+            System.out.println("I hate life");
+            this.createVaultItemViewModel.setState(new CreateVaultItemState(user.get(), userRepository.get()));
+            this.createVaultItemViewModel.onStateChanged();
+            this.viewManagerModel.setState(ViewConstants.CREATE_VAULT_ITEM_VIEW);
+            this.viewManagerModel.onStateChanged();
+        }
+        else {
+            // This is not reached
+        }
+        
     }
 }
