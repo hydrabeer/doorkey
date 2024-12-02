83cd54de79dbaf6abe14b2b0d10ecd111cd6ae4b
9naama <amaan.baweja@gmail.com>
2024-12-01 22:51:45 -0500

    added test for home iteractor, 100% coverage

---
+++ src/main/java/service/copy_credentials/TimeInputData.java
@@ -0,0 +1,13 @@
+package service.copy_credentials;
+
+public class TimeInputData {
+    private int time;
+
+    public TimeInputData(int time) {
+        this.time = time;
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
---
+++ src/test/java/home/HomeInteractorTest.java
@@ -0,0 +1,63 @@
+package home;
+
+import entity.AbstractVaultItem;
+import entity.PasswordVaultItem;
+import exception.InvalidVaultItemException;
+import mock.MockHomePresenter;
+import mock.MockLoginPresenter;
+import mock.MockRepositoryProvider;
+import mock.MockUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import repository.UserRepository;
+import service.home.HomeInputData;
+import service.home.HomeInteractor;
+import service.login.LoginInteractor;
+
+public class HomeInteractorTest {
+    private MockUserRepository mockUserRepository = new MockUserRepository();
+    private MockHomePresenter mockHomePresenter = new MockHomePresenter();
+    private MockRepositoryProvider repositoryProvider = new MockRepositoryProvider(mockUserRepository);
+    private HomeInteractor homeInteractor = new HomeInteractor(
+            repositoryProvider,
+            mockHomePresenter
+    );
+
+    @BeforeEach
+    public void setUp() {
+        mockUserRepository = new MockUserRepository();
+        mockHomePresenter = new MockHomePresenter();
+        mockUserRepository = new MockUserRepository();
+        homeInteractor = new HomeInteractor(
+                repositoryProvider,
+                mockHomePresenter
+        );
+    }
+
+    @Test
+    public void displayVaultItemTest() throws InvalidVaultItemException {
+        AbstractVaultItem item = new PasswordVaultItem(
+                "title", "username", "password", "https://www.example.com/");
+        HomeInputData inputData = new HomeInputData(item);
+        homeInteractor.displayVaultItem(inputData);
+        assert(mockHomePresenter.hasGeneralError("Show VaultView"));
+    }
+
+    @Test
+    public void signOutTest() {
+        homeInteractor.signOut();
+        assert(mockHomePresenter.hasGeneralError("Show LoginView"));
+    }
+
+    @Test
+    public void displayCreateVaultItemViewTest() {
+        homeInteractor.displayCreateVaultItemView();
+        assert(mockHomePresenter.hasGeneralError("Show CreateView"));
+    }
+
+    @Test
+    public void displayImportViewTest() {
+        homeInteractor.displayImportView();
+        assert(mockHomePresenter.hasGeneralError("Show ImportView"));
+    }
+}
