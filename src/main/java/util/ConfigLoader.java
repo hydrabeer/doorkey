bbcf846f01de35bc7e3640d644d5096c70430368
Saeed Al-Shrouf <realsaeedshrouf@gmail.com>
2024-12-01 19:19:28 -0500

    Added config loader to load API key for password gen

---
+++ src/main/java/util/ConfigLoader.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Utility class to load configuration properties.
+ */
+public class ConfigLoader {
+    private static final String CONFIG_FILE = "config.properties";
+    private final Properties properties;
+
+    public ConfigLoader() throws IOException {
+        properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream(CONFIG_FILE)) {
+            if (input == null) {
+                throw new IOException("Unable to find " + CONFIG_FILE);
+            }
+            properties.load(input);
+        }
+    }
+
+    /**
+     * Retrieves the value associated with the given key.
+     *
+     * @param key The property key.
+     * @return The property value.
+     */
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
